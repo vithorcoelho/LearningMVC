8408d9b91315a08844649697885c7a6acccb041d
João Vithor <j.vithor13@gmail.com>
2018-11-08 22:58:23 -0200

    Update App/init.php

---
+++ App/init.php
@@ -0,0 +1,13 @@
+<?php
+
+	class Init {
+
+	}
+
+	$ar['xpto'] = array('route' => '/xpto', 'controller' => 'xpto', 'action'=> 'index');
+
+	$rota['index'] = 'index';
+	$rota['index']['action'] = 'index';
+	$rota['index']['xpto'] = 'xpto';
+
+	//www.localhost.com/xpto
