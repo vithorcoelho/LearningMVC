50a8466033394837042027839a6c6c404d1e1de5
Joao <j.vithor13@gmail.com>
2019-03-10 01:55:13 -0300

    p

--- curso/1-namespace.php
+++ curso/1-namespace.php
@@ -3,22 +3,3 @@
 	// Namespace serve para evitar conflitos com nomes repetidos de classes
 	// As barras separam os diretorios
 	namespace SON\Xpto;
-
-	class Exemplo {
-
-	}
-
-	// Pode ser utilizado dest a forma
-	// As barras separa os diretorios
-	$exemplo = new \SON\Xpto\Exemplo;
-
-
-
-
-
-	//Também pode ser utilizado desta forma em outro arquivo
-	//"use" instancia a namespace
-	use SON\Xptoz\Exemplo;
-
-	//Instancia a classe
-	$exemplo2 = new Exemplo;
